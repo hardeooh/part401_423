1ed8cac03d5f17fce0b5fb281d95b32fd37a885e
hardeooh <harrydaleoh@gmail.com>
2023-04-30 01:04:17 -0700

    MVC model setup

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const morgan = require('morgan')
-const blogsRouter = require('./routes/blog')
-const config = require('./utils/config')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/notes', notesRouter)
 
-morgan.token('id', function getId (req) {
-  return req.id
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT} frontend`)
